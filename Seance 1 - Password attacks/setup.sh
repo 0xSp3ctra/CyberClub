7a264911d03f0e9eb0004f25ad5b32b27b5d579c
Colin Ledru <colin.ledru@gmail.com>
2023-12-04 10:14:18 +0100

    modified:   Seance 1 - Password attacks/Exos/exo3.jwt 	modified:   Seance 1 - Password attacks/setup.sh

--- Seance 1 - Password attacks/setup.sh
+++ Seance 1 - Password attacks/setup.sh
@@ -4,11 +4,9 @@ YE='\033[1;33m'
 RS='\033[0m'
 
 echo -e "${YE}[+] Installing required packets for the CyberClub${RS}"
-sudo apt update -y && sudo apt upgrade -y && sudo apt install -y git make gcc libssl-dev pip && pip3 install PyJWT tqdm
+sudo apt update -y && sudo apt install -y pip && 
+pip3 install tqdm --break-system-packages
+pip3 install PyJWT --break-system-packages
 
 echo -e "${YE}[+] Installing hashid${RS}"
 sudo apt install hashid -y
-
-echo -e "${YE}[+] Cloning the CyberClub repo with the exercices${RS}"
-git clone https://github.com/0xSp3ctra/CyberClub.git && cd
-mv -v CyberClub /home/*/
