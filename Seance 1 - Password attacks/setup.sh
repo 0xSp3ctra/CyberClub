771720fc51543166ff2bd025598bf0841696bb4b
Colin Ledru <colin.ledru@gmail.com>
2023-12-05 00:05:30 +0100

    modified:   Seance 1 - Password attacks/Exos/exos.txt 	modified:   Seance 1 - Password attacks/setup.sh

--- Seance 1 - Password attacks/setup.sh
+++ Seance 1 - Password attacks/setup.sh
@@ -4,7 +4,7 @@ YE='\033[1;33m'
 RS='\033[0m'
 
 echo -e "${YE}[+] Installing required packets for the CyberClub${RS}"
-sudo apt update -y && sudo apt install -y pip
+sudo apt update -y && sudo apt install -y pip hashcat keepassxc
 
 echo -e "${YE}[+] Installing hashid${RS}"
 sudo apt install hashid -y
