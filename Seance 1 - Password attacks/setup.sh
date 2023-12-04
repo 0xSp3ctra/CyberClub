0f04fec3fb5afaff6d16387106dc46a08a9b778c
Colin Ledru <84533475+0xSp3ctra@users.noreply.github.com>
2023-12-04 10:51:13 +0100

    Add files via upload

--- Seance 1 - Password attacks/setup.sh
+++ Seance 1 - Password attacks/setup.sh
@@ -4,9 +4,9 @@ YE='\033[1;33m'
 RS='\033[0m'
 
 echo -e "${YE}[+] Installing required packets for the CyberClub${RS}"
-sudo apt update -y && sudo apt install -y pip && 
-pip3 install tqdm --break-system-packages
-pip3 install PyJWT --break-system-packages
+sudo apt update -y && sudo apt install -y pip
+pip install tqdm --break-system-packages
+pip install PyJWT --break-system-packages
 
 echo -e "${YE}[+] Installing hashid${RS}"
 sudo apt install hashid -y
