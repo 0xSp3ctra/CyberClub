fb85c9acc71e6ce5b9db6703520c2208e4b63f62
Colin Ledru <colin.ledru@gmail.com>
2023-12-04 22:41:43 +0100

    modified:   Seance 1 - Password attacks/setup.sh

--- Seance 1 - Password attacks/setup.sh
+++ Seance 1 - Password attacks/setup.sh
@@ -5,8 +5,9 @@ RS='\033[0m'
 
 echo -e "${YE}[+] Installing required packets for the CyberClub${RS}"
 sudo apt update -y && sudo apt install -y pip
-pip install tqdm --break-system-packages
-pip install PyJWT --break-system-packages
 
 echo -e "${YE}[+] Installing hashid${RS}"
 sudo apt install hashid -y
+
+pip install tqdm --break-system-packages
+pip install PyJWT --break-system-packages
